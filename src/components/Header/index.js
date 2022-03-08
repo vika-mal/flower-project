4d5d2c4e92c401bcef60640c09efb567caee162e
vika-mal <vkiryash@mail.ru>
2022-03-08 16:34:37 +0300

    edit header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import s from "./Header.module.scss";
 import logo from "../../images/logo.svg";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Header = () => {
+  const header = useRef(null);
+  const stickyBlock = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener("scroll", (e) => {
+      if (stickyBlock.current.getBoundingClientRect().top < 0) {
+        stickyBlock.current.classList.add(s.ad);
+        console.log(stickyBlock.current.getBoundingClientRect().bottom);
+        console.log(stickyBlock.current.classList);
+      }
+    });
+  }, [stickyBlock]);
   return (
-    <div className={s.header}>
+    <div ref={header} className={s.header}>
       <div className="container">
         <div className={s.header__wrapper}>
           <a href="/" className={s.logo}>
@@ -44,6 +56,17 @@ const Header = () => {
                 </a>
               </li>
             </ul>
+          </div>
+          <div ref={stickyBlock} className={s["stickyBlock"] + " " + s.lite}>
+            <div className={s.stickyBlock__wrapper}>
+              <a className={s.stickyBlock__tel} href="tel:+375 (29) 113-69-69">
+                +375 (29) 113-69-69
+              </a>
+              <button className={s.stickyBlock__callback + ' ' + s.lite}> заказать звонок </button>
+              <button className={s.stickyBlock__basket}>
+                <span className={s.stickyBlock__basket__num}>0</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
